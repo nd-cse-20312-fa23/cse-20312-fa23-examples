794daceaf5a5facdadae31ac15dd5b2e7b73d7bf
Peter Bui <pbui@nd.edu>
2023-08-17 13:44:19 -0400

    Lecture 05: update str_length

--- lecture05/str_length.c
+++ lecture05/str_length.c
@@ -1,8 +1,9 @@
 /* str_length.c */
 
 #include <assert.h>
-#include <string.h>
+#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 size_t str_length(const char *s) {
     const char *c;
@@ -13,6 +14,14 @@ size_t str_length(const char *s) {
 int main(int argc, char *argv[]) {
     for (int i = 1; i < argc; i++) {
     	assert(str_length(argv[i]) == strlen(argv[i]));
+    	printf("%lu: %s\n", str_length(argv[i]), argv[i]);
     }
+
+    char buffer[BUFSIZ];
+    while (fgets(buffer, BUFSIZ, stdin)) {
+    	assert(str_length(buffer) == strlen(buffer));
+    	printf("%lu: %s", str_length(buffer), buffer);
+    }
+
     return EXIT_SUCCESS;
 }
