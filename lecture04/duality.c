83e1aba70f4006f8fbd84a19ebfa5f2ddf74d777
Peter Bui <pbui@nd.edu>
2023-08-17 13:19:19 -0400

    Lecture 04: update duality

--- lecture04/duality.c
+++ lecture04/duality.c
@@ -4,6 +4,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void dump(int *a, int n) {
+    // WRONG: n = sizeof(a) / sizeof(int);
+    for (size_t i = 0; i < n; i++) {
+    	printf("%ld. %d\n", i, a[i]);
+    }
+    
+    // POINTERS: only
+    // SENTINEL: for (int *p = a; *p >= 0; p++) {
+    for (int *p = a; p < (a + n); p++) {
+    	printf("%ld. %d\n", p - a, *p);
+    }
+}
+
 int main(int argc, char *argv[]) {
     int a[]  = {5, 4, 7, 0, 1, -1};
     size_t n = sizeof(a) / sizeof(int);
@@ -14,11 +27,7 @@ int main(int argc, char *argv[]) {
     	assert(a[i]  == i[a]);
 	assert(a + i == &a[i]);
     }
-    
-    // for (int *p = a; p < (a + n); p++) {
-    for (int *p = a; *p >= 0; p++) {
-    	printf("%d\n", *p);
-    }
 
+    dump(a, n);
     return EXIT_SUCCESS;
 }
