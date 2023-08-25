8858587cefd50cde3abea2dd0396504b856a1458
Peter Bui <pbui@nd.edu>
2023-08-25 16:27:53 -0400

    Lecture 07: more comments

--- lecture07/array.c
+++ lecture07/array.c
@@ -85,11 +85,13 @@ void	array_remove(Array *array, size_t index) {
 /* Main Execution */
 
 int main(int argc, char *argv[]) {
+    // Create Array
     Array* array = array_create();
     
     printf("array->size     = %lu\n", array->size);
     printf("array->capacity = %lu\n", array->capacity);
 
+    // Append to Array
     array_append(array, 4);
     array_append(array, 6);
     array_append(array, 5);
@@ -99,15 +101,18 @@ int main(int argc, char *argv[]) {
     printf("array->size     = %lu\n", array->size);
     printf("array->capacity = %lu\n", array->capacity);
 
+    // Access elements in Array
     for (size_t i = 0; i < array->size; i++) {
     	printf("array[%lu] = %d\n", i, array_at(array, i));
     }
 
+    // Search for elements in Array
     printf("array_index(4) = %ld\n", array_index(array, 4));
     printf("array_index(5) = %ld\n", array_index(array, 5));
     printf("array_index(6) = %ld\n", array_index(array, 6));
     printf("array_index(7) = %ld\n", array_index(array, 7));
 
+    // Remove elements from Array
     array_remove(array, 1);
     array_remove(array, 2);
     
