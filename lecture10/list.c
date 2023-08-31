0ed2069d045cef45e5d4ef234872ffb58e6cac8b
Peter Bui <pbui@nd.edu>
2023-08-30 21:26:12 -0400

    Lecture 10: add Linked List example

---
+++ lecture10/list.c
@@ -0,0 +1,51 @@
+/* Lecture 10: Linked List */
+
+#include "list.h"
+
+/* Node Structure */
+
+Node *	node_create(int data, Node *next) {
+    Node *n = malloc(sizeof(Node));
+    if (n) {
+    	n->data = data;
+    	n->next = next;
+    }
+    return n;
+}
+
+/* List Structure */
+
+void	list_push(List *l, int value) {
+    l->head = node_create(value, l->head);
+}
+
+int	list_pop(List *l) {
+    Node *n = l->head;
+    l->head = l->head->next;
+
+    int value = n->data;
+    free(n);
+    return value;
+}
+
+ssize_t	list_index(List *l, int value) {
+    ssize_t index = 0;
+    for (Node *curr = l->head; curr; curr = curr->next) {
+    	if (curr->data == value)
+    	    return index;
+    	index++;
+    }
+    return -1;
+}
+
+int	list_at(List *l, size_t index) {
+    size_t cindex = 0;
+    Node  *curr   = l->head;
+
+    while (curr && cindex != index) {
+    	curr = curr->next;
+    	cindex++;
+    }
+
+    return (curr && cindex == index) ? curr->data : -1;
+}
---
+++ lecture10/list.h
@@ -0,0 +1,28 @@
+/* Lecture 10: Linked List */
+
+#pragma once
+
+#include <stdbool.h>
+#include <stdlib.h>
+
+/* Node Structure */
+
+typedef struct Node Node;
+struct Node {
+    int   data;	    // Value associated with Node
+    Node *next;	    // Pointer to next Node in List
+};
+
+Node *	node_create(int data, Node *next);
+
+/* List Structure */
+
+typedef struct {
+    Node *head;	    // Pointer to first Node in List
+} List;
+
+void	list_push(List *l, int value);
+int	list_pop(List *l);
+
+ssize_t	list_index(List *l, int value);
+int	list_at(List *l, size_t index);
