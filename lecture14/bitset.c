628ed7eff362aba76ad5ab9479b045c4b2da4c32
Peter Bui <pbui@nd.edu>
2023-09-11 09:01:12 -0400

    Lecture 14: use long int

--- lecture14/bitset.c
+++ lecture14/bitset.c
@@ -8,15 +8,15 @@
 
 void	bitset_add(Bitset *b, int value) {
     assert(value < sizeof(Bitset)*8);
-    *b |= (1<<value);
+    *b |= (1L<<value);
 }
 
 bool	bitset_contains(Bitset *b, int value) {
     assert(value < sizeof(Bitset)*8);
-    return *b & (1<<value);
+    return *b & (1L<<value);
 }
 
 void	bitset_remove(Bitset *b, int value) {
     assert(value < sizeof(Bitset)*8);
-    *b &= ~(1<<value);
+    *b &= ~(1L<<value);
 }
