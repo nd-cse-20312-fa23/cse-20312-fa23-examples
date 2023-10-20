fc44064d28b5eef4d4f4dd8fffb7c7fa7001e656
Peter Bui <pbui@nd.edu>
2023-10-20 09:42:25 -0400

    Lecture 23: update duplicates

--- lecture23/duplicates.py
+++ lecture23/duplicates.py
@@ -14,7 +14,7 @@ def make_random_array(n=NUMBERS_MAX):
         for _ in range(n)
     ]
 
-def has_duplicates(numbers):
+def has_duplicates_set(numbers):
     seen = set()
     for number in numbers:
         if number in seen:
@@ -22,10 +22,18 @@ def has_duplicates(numbers):
         seen.add(number)
     return False
 
+def has_duplicates_sorting(numbers):
+    numbers.sort()
+    for index in range(1, len(numbers)):
+        if numbers[index] == numbers[index - 1]:
+            return True
+    return False
+
 # Main Execution
 
 def main():
-    numbers = make_random_array()
+    has_duplicates = has_duplicates_set
+    numbers        = make_random_array()
     print("Yeah" if has_duplicates(numbers) else "Nope")
 
 if __name__ == '__main__':
