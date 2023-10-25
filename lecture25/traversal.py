b7baeb26b243b22343b590fc0bd0d919a328d350
Peter Bui <pbui@nd.edu>
2023-10-24 21:34:57 -0400

    Lecture 25: add traversal

---
+++ lecture25/traversal.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+
+''' traversal.py
+
+This demonstrates traversing a tree using DFS and BFS.
+'''
+
+from dataclasses import dataclass
+from collections import deque
+
+# Classes
+
+@dataclass
+class Node:
+    value:  str
+    left:   'Node' = None
+    right:  'Node' = None
+
+# Functions
+
+def dfs(root):
+    ''' Traverse binary tree in depth-first order '''
+    if not root: return                         # Base case: No Node
+
+    dfs(root.left)                              # Recursive: Visit left branch
+    print(root.value)                           # Visit current
+    dfs(root.right)                             # Recursive: Visit right branch
+
+def bfs(root):
+    ''' Traverse binary tree in breadth-first order '''
+    queue = deque([root])                       # Queue of nodes to visit
+
+    while queue:
+        node = queue.popleft()                  # Remove next node to visit
+        if not node: continue                   # Skip invalid node
+
+        print(node.value)                       # Visit node
+        queue.append(node.left)                 # Add left child
+        queue.append(node.right)                # Add right child
+
+# Main Execution
+
+def main():
+    tree = Node('B', Node('I', Node('A'), Node('R')), Node('N', Node('Y')))
+
+    dfs(tree)
+    print()
+    bfs(tree)
+
+if __name__ == '__main__':
+    main()
